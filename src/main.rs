0331a55626d1f4f7bc9f65a95c02ab5913d99f3e
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-04-16 09:42:05 -0300

    feat: Simple spaceship wireframe model rendering

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,40 @@ fn main() {
         pos: Vector2::new(10.0, 10.0),
         dir: Vector2::new(0.0, 0.0),
         angle: 0f64,
-        size: 1,
+        size: 0,
     };
+
+    let ship_model: [Vector2; 3] = [
+        Vector2::new(0.0,  -6.5),
+        Vector2::new(-2.5, 3.5),
+        Vector2::new(2.5,  3.5),
+    ];
 
 
     logic.run(move |screen, keys, dt| {
+
+        let mut model = ship_model; 
+
+        for (i, point) in ship_model.iter().enumerate() {
+            // rotate
+            model[i] = Vector2::new(
+                point.x * player.angle.cos() - point.y * player.angle.sin(),
+                point.x * player.angle.sin() + point.y * player.angle.cos(),
+            );
+            // translate
+            model[i] += player.pos
+        }
+
+        let k = ship_model.len();
+        for i in 0..=k {
+            let j = i + 1;
+            screen.draw_line(
+                model[i % k],
+                model[j % k],
+                Color::WHITE
+            )
+        }
+
         if keys[Scancode::W] {
             player.dir.x += player.angle.sin() * 0.05 * dt;
             player.dir.y -= player.angle.cos() * 0.05 * dt;
